305d70dbda1acbf2b943c1bf0abdfb8c6a5414b2
Arigbede Omobolaji <arigbedeomobolaji@gmail.com>
2020-11-23 12:08:57 +0100

    fourth commit

--- public/js/index.js
+++ public/js/index.js
@@ -12,6 +12,10 @@ submit.addEventListener("click", async (e) => {
 
  const response = await fetch(url);
  const data = await response.json();
- weatherForecast.innerHTML = `<h1>Here is your weather report</h1> <p>${data.weatherDesc}<p>`;
+ if (!data.error) {
+  weatherForecast.innerHTML = `<h1>Here is your weather report</h1> <p>${data.weatherDesc}<p>`;
+ } else {
+  weatherForecast.innerHTML = `<h1 style="font-size:16px;color:red;background:rgba(255,255,255,.4);margin-top:10px">Oops Weather information not found for location</h1>`;
+ }
  address.value = "";
 });
